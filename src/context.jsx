beee1dafcfa893b59dee1c304f9852e3f24a7e84
Walusimbi Herbert Henry <walherb@gmail.com>
2023-07-24 13:56:48 +0300

    first commit

---
+++ src/context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches;
+  const storeDarkMode = localStorage.getItem('darkTheme') === 'trues';
+  return storeDarkMode || prefersDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState('dog');
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
